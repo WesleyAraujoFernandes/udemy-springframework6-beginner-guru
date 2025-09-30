353fce245b199714d2ef6e44ae9add32ff35be1b
WesleyAraujoFernandes <wesley.a.fernandes@gmail.com>
2025-09-30 15:57:31 -0300

    autor and book models createad

---
+++ src/main/java/guru/springframework/spring6webapp/domain/Book.java
@@ -0,0 +1,21 @@
+package guru.springframework.spring6webapp.domain;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)    
+    private Long id;
+    private String title;
+    private String isbn;
+
+    private Set<Author> authors;
+}
