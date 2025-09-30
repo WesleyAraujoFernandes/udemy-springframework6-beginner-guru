b41d0e358933630f452b35b2fd04099083c52588
WesleyAraujoFernandes <wesley.a.fernandes@gmail.com>
2025-09-30 18:05:02 -0300

    bootstrap and repositories created

--- src/main/java/guru/springframework/spring6webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Author.java
@@ -6,16 +6,25 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Entity
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @EqualsAndHashCode.Include
+    @ToString.Include
     private Long id;
+    @ToString.Include
     private String firstname;
+    @ToString.Include
     private String lastname;
-
+    @ManyToMany(mappedBy = "authors")
+    //@ToString.Include
     private Set<Book> books;
 }
--- src/main/java/guru/springframework/spring6webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Book.java
@@ -6,17 +6,29 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Entity
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)    
+    @EqualsAndHashCode.Include
+    @ToString.Include
     private Long id;
+    @ToString.Include
     private String title;
+    @ToString.Include
     private String isbn;
-
+    @ManyToMany
+    @JoinTable(name = "author_book", 
+        joinColumns = @jakarta.persistence.JoinColumn(name = "book_id"), 
+        inverseJoinColumns = @jakarta.persistence.JoinColumn(name = "author_id"))
     private Set<Author> authors;
-    
 }
